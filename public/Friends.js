51b9ce66445f14f60cb41b10ff5fc336d947da0f
Ankit Tyagi <ancorp007@gmail.com>
2024-12-03 23:20:11 +0530

    first commit

---
+++ public/Friends.js
@@ -0,0 +1,18 @@
+const Friends =[
+    {
+        "Name" : "Ankit Tyagi",
+        "imageUrl" : "https://avatar.iran.liara.run/username?username=Ankit%20Tyagi"   ,
+        "Balance"  : 20
+    },
+    {
+        "Name" : "Aakash Garg",
+        "imageUrl" : "https://avatar.iran.liara.run/username?username=Aakash%20Garg"   ,
+        "Balance"  : 10
+    },
+    {
+        "Name" : "Ayush Gwari",
+        "imageUrl" : "https://avatar.iran.liara.run/username?username=Ayush%20Gwari"   ,
+        "Balance"  : -7
+    },
+]
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const Friends = [
+  {
+    Name: "Ankit Tyagi",
+    imageUrl: "https://avatar.iran.liara.run/public",
+    Balance: 20,
+  },
+  {
+    Name: "Aakash Garg",
+    imageUrl: "https://avatar.iran.liara.run/public",
+    Balance: 10,
+  },
+  {
+    Name: "Ayush Gwari",
+    imageUrl: "https://avatar.iran.liara.run/public",
+    Balance: -7,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="app">
+        <FriendList></FriendList>
+      </div>
+      <div className="app2">
+      <FormAddFreind></FormAddFreind>
+      <Button>Add Friend</Button>
+      </div>
+    </>
+  );
+}
+
+function FriendList() {
+  return (
+    <div>
+      {Friends.map((item, index) => {
+        return <Friend key={index} pro={item}></Friend>;
+      })}
+    </div>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function Friend(props) {
+  const { Name, imageUrl, Balance } = props.pro;
+  return (
+    <div className="friend">
+      <div className="friend-hero">
+        <img src={imageUrl}></img>
+        <div className="friend-data">
+          <p>{Name}</p>
+          {Balance > 0 && (
+            <p className="green">you get amount Rs.{Math.abs(Balance)}</p>
+          )}
+          {Balance < 0 && (
+            <p className="red">you owe amount Rs.{Math.abs(Balance)}</p>
+          )}
+          {Balance == 0 && <p>you are all settle up.</p>}
+        </div>
+      </div>
+      <div>
+        <Button>Select</Button>
+      </div>
+    </div>
+  );
+}
+
+function FormAddFreind() {
+  return (
+    <div className="form">
+      <div className="form-data">
+        <div className="form-field">
+          <label> 👯 Friend Name:</label>
+          <input type="text"></input>
+        </div>
+          <Button>Submit</Button>
+          <Button>Close</Button>
+      </div>
     </div>
   );
 }
